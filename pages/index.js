9324b1ba1c3afcd93e93373fb5b7c0de7c548361
springlordpants <springlordpants@gmail.com>
2020-10-30 14:10:38 -0700

    Organizing list function using hooks

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,31 @@
 import React from 'react'
 
-class List extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      filterItems: this.props.items
-    };
 
-    this.filterItems = this.filterItems.bind(this);
+const List = ({items}) => {
+  const [filteredItems, setFilteredItems] = React.useState(items);
+
+
+const filterItems = (e) => {
+  const searchValue = e.target.value;
+  const currentItems = [...items];
+  const matchingItems = currentItems.filter((item) => item.startsWith(searchValue));
+  
+  setFilteredItems(matchingItems);
+
   }
 
-  filterItems(e) {
-    const searchValue = e.target.value;
-    const currentItems = [...this.props.items];
-    const matchingItems = currentItems.filter((item) => item.startsWith(searchValue));
-    
-    this.setState({
-      filterItems: matchingItems
-    });
-  }
+  return (
+    <>
+      <input onChange={filterItems} />
+      <ul>
+        {filteredItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 
-  render () {
-    return (
-      <>
-        <input onChange={this.filterItems} />
-        <ul>
-          {this.state.filterItems.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      </>
-    )
-  }
-}
-
+};
 const ListContainer = () =>
   <List items = {['Learn React', 'Learn Next.js', '???', 'Profit']} />
 
