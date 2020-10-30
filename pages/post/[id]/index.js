5388fb43665041943c2f5186bb9e4a975b08b8bc
springlordpants <springlordpants@gmail.com>
2020-10-30 15:27:19 -0700

    get Initial props example

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,16 +1,18 @@
-import {useRouter} from 'next/router';
 import Header from '../../../components/header'
 
-const Post = () => {
-    const router = useRouter();
-    const {id} = router.query;
-
-    return (
+const Post = ({title, body}) => (
         <>
           <Header />
-          <h1>Post: {id}</h1>
+          <h1>{title}</h1>
+          <p>{body}</p>
         </>
     );
-};
+
+Post.getInitialProps = async ({query}) => {
+    const res =  await fetch(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+    const post =  res.json();
+
+    return post;
+}
 
 export default Post;
