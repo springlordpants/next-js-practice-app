d7b64c5e2608a7bdf7ae60d44e2179edd2270d04
springlordpants <springlordpants@gmail.com>
2020-10-30 12:27:59 -0700

    Using props to filter items

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,39 @@
 import React from 'react'
 
-const List = ({items}) => (
-  <ul>
-    {items.map((item) => (
-      <li key={item}>{item}</li>
-    ))}
-  </ul>
-);
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      filterItems: this.props.items
+    };
+
+    this.filterItems = this.filterItems.bind(this);
+  }
+
+  filterItems(e) {
+    const searchValue = e.target.value;
+    const currentItems = [...this.props.items];
+    const matchingItems = currentItems.filter((item) => item.startsWith(searchValue));
+    
+    this.setState({
+      filterItems: matchingItems
+    });
+  }
+
+  render () {
+    return (
+      <>
+        <input onChange={this.filterItems} />
+        <ul>
+          {this.state.filterItems.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </>
+    )
+  }
+}
 
 const ListContainer = () =>
   <List items = {['Learn React', 'Learn Next.js', '???', 'Profit']} />
