59c85303d3725cbfcea3800f5685ff15a5571d7e
springlordpants <springlordpants@gmail.com>
2020-10-30 14:57:59 -0700

    Page navigation set up

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,8 @@
-import React from 'react'
+import Header from '../components/header'
 
-const List = ({ items, ...props }) => {
-  const [filteredItems, setFilteredItems] = React.useState(items)
-
-  const filterItems = (e) => {
-    const searchValue = e.target.value
-    const currentItems = [...items]
-    const matchingItems = currentItems.filter((item) => item.startsWith(searchValue))
-
-    setFilteredItems(matchingItems)
-  }
-
-  return (
-    <>
-      <input onChange={filterItems} />
-      <ul {...props}>
-        {filteredItems.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-    </>
-  )
+export default function Home() {
+  return <div>
+    <Header></Header>
+    <h2>Welcome to my Home Page!</h2>
+  </div>
 }
-const ListContainer = () =>
-  <List aria-label='My Fancy List' items={['Learn React', 'Learn Next.js', '???', 'Profit']} />
-
-export default ListContainer
